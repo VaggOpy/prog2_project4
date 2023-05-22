dfeaa3f347b31e9fc2748525253f4c9c58f4f5d3
vaggelisplytas <vaggelis.plytas@gmail.com>
2023-05-22 17:13:22 +0300

    created a new myread() that works perfectly

--- p4diff.c
+++ p4diff.c
@@ -20,34 +20,61 @@ int find_max(int num1, int num2) {
     return num2;
 }
 
-char *myread(char str[BLOCK], int **count_bytes_read, FILE *stream) {
-    int check = fread(str, BLOCK, BLOCK, stream);
-    *count_bytes_read += check;
+// char *myread(char str[BLOCK+1], int *count_bytes_read, FILE *stream) {
+//     int check = fread(str, sizeof(char), 1, stream);
+//     *count_bytes_read += check;
 
-    if (check < BLOCK) str[check] = '\0';
-    if (check == 0) {
-        char new[BLOCK] = {'\0'};
+//     if (check <= BLOCK) str[check] = '\0';
+//     if (check == 0) {
+//         char new[BLOCK+1] = {'\0'};
+//         strcpy(str, new);
+//     }
+
+//     return str;
+// }
+char *myread(char str[BLOCK+1], int *count_bytes_read, FILE *stream) {
+    char *character = malloc(sizeof(char));
+    int check, i;
+
+    for (i = 0; i < BLOCK; i++) {
+        check = fread(character, sizeof(char), 1, stream);
+        if (check == 0) break;
+        if (*character == '\r') {
+            i--;
+            continue;
+        }
+
+        str[i] = *character;
+        (*count_bytes_read)++;
+    }
+
+    if (i <= BLOCK) str[i] = '\0';
+    if (i == 0) {
+        char new[BLOCK+1] = {'\0'};
         strcpy(str, new);
     }
 
+    free(character);
     return str;
 }
 
 int main(int argc, char *argv[]) {
-    char pipe_read[BLOCK], expected_read[BLOCK], str1[BLOCK], str2[BLOCK];
+    char pipe_read[BLOCK+1], expected_read[BLOCK+1], str1[BLOCK+1], str2[BLOCK+1];
+    FILE *expected_out_fd = fopen(argv[1], "r");
+    if (expected_out_fd == NULL) return -1;
+
     int *total_expected_bytes = (int *) malloc(sizeof(int)), matching_bytes = 0;
     int *total_pipe_bytes = (int *) malloc(sizeof(int)), max;
     *total_expected_bytes = 0, *total_pipe_bytes = 0;
 
-    FILE *expected_out_fd = fopen(argv[1], "r");
-    if (expected_out_fd == NULL) return -1;
-
     // Counts how many similar bytes <progname>.out and the pipe have
     while (1) {
-        strcpy(pipe_read, myread(str1, &total_pipe_bytes, stdin));
-        strcpy(expected_read, myread(str2, &total_expected_bytes, expected_out_fd));
+        strcpy(pipe_read, myread(str1, total_pipe_bytes, stdin));
+        strcpy(expected_read, myread(str2, total_expected_bytes, expected_out_fd));
+        if (pipe_read[0] == '\0') break;
 
-        printf("%s %s", pipe_read, expected_read);
+        // printf("%c", expected_read[BLOCK-1]);
+        // printf("%s\n%s\n", pipe_read, expected_read);
         for (int i = 0; i < BLOCK && (pipe_read[i] != '\0'); i++) {
             if (pipe_read[i] == expected_read[i]) matching_bytes++; 
         }
@@ -55,10 +82,12 @@ int main(int argc, char *argv[]) {
     fclose(expected_out_fd);
 
     max = find_max(*total_expected_bytes, *total_pipe_bytes);
+    printf("mybytes:%d expected_bytes:%d same:%d max:%d\n", *total_pipe_bytes, *total_expected_bytes, matching_bytes, max);
+    printf("%d\n", matching_bytes*100 / max);
+
     free(total_expected_bytes);
     free(total_pipe_bytes);
 
-    // printf("mybytes:%d expected_bytes:%d same:%d max:%d\n", total_pipe_bytes, total_expected_bytes, matching_bytes, max);
     if (max == 0) return 0;
     return matching_bytes*100 / max;
 }
