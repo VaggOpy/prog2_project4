5f7a2abb4beab729fa0cf6e4f6d866a766a985a0
vaggelisplytas <vaggelis.plytas@gmail.com>
2023-05-29 17:54:09 +0300

    should be last update | perfect score at Test4

--- p4diff.c
+++ p4diff.c
@@ -20,51 +20,46 @@ int find_max(int num1, int num2) {
     return num2;
 }
 
-char *myread(char str[BLOCK+1], int *count_bytes_read, FILE *stream) {
-    for (int i = 0; i <= BLOCK; i++) str[i] = '\0';
-
-    int check = fread(str, sizeof(char), BLOCK, stream);
+char *myread(char str[BLOCK], int *count_bytes_read, FILE *stream) {
+    int check = fread(str, sizeof(char), BLOCK-1, stream);
     system_call_failure(check);
-
-    *count_bytes_read += check;
-    str[check] = '\0';
+    *count_bytes_read = check;
 
     return str;
 }
 
 int main(int argc, char *argv[]) {
-    char pipe_read[BLOCK+1], expected_read[BLOCK+1], str1[BLOCK+1], str2[BLOCK+1];
+    char *pipe_read = (char *) malloc(sizeof(char) * BLOCK);
+    char *expected_read = (char *) malloc(sizeof(char) * BLOCK);
     FILE *expected_out_fd = fopen(argv[1], "r");
     if (expected_out_fd == NULL) return -1;
 
-    int *total_expected_bytes = (int *) malloc(sizeof(int)), matching_bytes = 0;
-    int *total_pipe_bytes = (int *) malloc(sizeof(int)), max, check;
-    *total_expected_bytes = 0, *total_pipe_bytes = 0;
+    int *num_out = (int *) malloc(sizeof(int)), *num_pipe = (int *) malloc(sizeof(int));
+    int total_pipe_bytes = 0, total_output_bytes = 0, max, check, matching_bytes = 0;
+    *num_out = 0, *num_pipe = 0;
 
     // Counts how many similar bytes <progname>.out and the pipe have
     while (1) {
-        for (int i = 0; i <= BLOCK; i++) {
-            pipe_read[i] = '\0';
-            expected_read[i] = '\0';
-        }
+        myread(pipe_read, num_pipe, stdin);
+        myread(expected_read, num_out, expected_out_fd);
+        total_pipe_bytes += *num_pipe;
+        total_output_bytes += *num_out;
+        if (*num_pipe == 0 && *num_out == 0) break;
 
-        strcpy(pipe_read, myread(str1, total_pipe_bytes, stdin));
-        strcpy(expected_read, myread(str2, total_expected_bytes, expected_out_fd));
-        if (pipe_read[0] == '\0' && expected_read[0] == '\0') break;
-
-        for (int i = 0; i <= BLOCK && pipe_read[i] != '\0'; i++) {
-            if (pipe_read[i] == expected_read[i]) {
-                matching_bytes++;
-            }
+        for (int i = 0; i < *num_pipe && i < *num_out; i++) {
+            if (pipe_read[i] == expected_read[i]) matching_bytes++;
         }
     }
     check = fclose(expected_out_fd);
     system_call_failure(check);
 
-    max = find_max(*total_expected_bytes, *total_pipe_bytes);
-    // printf("total_pipe: %d, total_expected: %d, matching: %d", *total_pipe_bytes, *total_expected_bytes, matching_bytes);
-    free(total_expected_bytes);
-    free(total_pipe_bytes);
+    max = find_max(total_pipe_bytes, total_output_bytes);
+    // printf("total_pipe: %d, total_expected: %d, matching: %d", total_pipe_bytes, total_output_bytes, matching_bytes);
+    free(num_out);
+    free(num_pipe);
+
+    free(pipe_read);
+    free(expected_read);
 
     if (max == 0) return 100;
     return matching_bytes*100 / max;
